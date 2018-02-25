a37b5a0fd1a68389242ac58c61a42a6fb6c0f3b2
CSkin <colinskinner4@gmail.com>
2018-02-25 17:34:26 -0600

    Add base files

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # morse-sending
 Prototype of Morse Harbor sending mechanic.
+Created with [Vue.js](https://vuejs.org/) and [jQuery](http://jquery.com/).
 
-Morse Harbor is an educational game about Morse code. To learn more, check out morse-harbor-gdd-v2.pdf above.
+Morse Harbor is an educational game about Morse code.
+To learn more, check out the [game design document](morse-harbor-gdd-v2.pdf).
---
+++ script.js
@@ -0,0 +1,25 @@
+$( document ).ready( function () {
+
+// -------------------------{  Vue Components  }-------------------------
+
+
+
+// --------------------------{  Vue Instance  }--------------------------
+
+var App = new Vue ({
+  el: '#app',
+  data: {},
+  computed: {},
+  methods: {},
+  components: {}
+});
+
+// --------------------------{  Key Handling  }--------------------------
+
+function keyHandler () {
+
+}
+
+$( document ).keyUp( keyHandler );
+
+});
